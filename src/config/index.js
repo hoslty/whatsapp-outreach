00c33aa323eec5ba6552e814fa42a58c23050031
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2026-08-24 23:14:19 -0300

    .

--- src/config/index.js
+++ src/config/index.js
@@ -11,11 +11,11 @@ const config = {
   dataPath: process.env.DATA_PATH || './data/contatos.json',
   controlFilePath: process.env.CONTROL_FILE_PATH || './data/contkrol.json',
   businessHours: {
-    windows: parseWindows(process.env.BUSINESS_HOURS_WINDOWS || '09:00-13:00,14:00-16:30'),
+    windows: parseWindows(process.env.BUSINESS_HOURS_WINDOWS || '09:00-11:30,14:00-16:30'),
   },
   interval: {
-    minMinutes: Number(process.env.MIN_INTERVAL_MINUTES || 2),
-    maxMinutes: Number(process.env.MAX_INTERVAL_MINUTES || 4),
+    minMinutes: Number(process.env.MIN_INTERVAL_MINUTES || 1),
+    maxMinutes: Number(process.env.MAX_INTERVAL_MINUTES || 2),
   },
   logDir: process.env.LOG_DIR || './logs',
 };
